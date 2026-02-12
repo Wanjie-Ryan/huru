791a3b87fe2632f2e9462acf2f6b9a9bed811e5a
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2026-02-12 20:29:10 +0300

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -4,6 +4,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = (url) => {
   console.log(url)
+  console.log("mongodb db logging...")
   // logging to see the urls 
   return mongoose.connect(url);
 };
