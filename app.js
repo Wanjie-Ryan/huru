2007ec946a00cfed28293bf2c25a2b6351b35962
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2026-02-19 06:21:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const DBConnection = async () => {
     await connectionDB(process.env.mongo_url);
 
     app.listen(port, () => {
-      console.log("ji")
+      console.log("jis")
       console.log(`server is running on port, ${port}`);
     });
   } catch (err) {
